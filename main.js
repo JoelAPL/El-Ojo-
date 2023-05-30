658d94ce87b83a5a37c5c28891150f82e91ec881
JoelAPL <joel.ale.programmer@gmail.com>
2023-05-30 15:51:39 -0500

    Correción de el otro ojo que no funcionaba

--- main.js
+++ main.js
@@ -1,19 +1,13 @@
-let punto1 = document.querySelector(".punto1 .punto2");
+let punto1 = document.querySelector(".punto1");
 let ojo1 = document.querySelector(".ojo1");
-
-document.onmousemove = (e)=>{
-    let x = e.clientX *100 / window.innerWidth + "%";
-    let y = e.clientY *100 / window.innerHeight + "%";
-    punto1.style.left = x;
-    punto1.style.top = y;
-}
-
 let punto2 = document.querySelector(".punto2");
 let ojo2 = document.querySelector(".ojo2");
 
-document.onmousemove = (e)=>{
-    let x = e.clientX *100 / window.innerWidth + "%";
-    let y = e.clientY *100 / window.innerHeight + "%";
-    punto2.style.left = x;
-    punto2.style.top = y;
-}
+document.onmousemove = (e) => {
+  let x = e.clientX * 100 / window.innerWidth + "%";
+  let y = e.clientY * 100 / window.innerHeight + "%";
+  punto1.style.left = x;
+  punto1.style.top = y;
+  punto2.style.left = x;
+  punto2.style.top = y;
+};
